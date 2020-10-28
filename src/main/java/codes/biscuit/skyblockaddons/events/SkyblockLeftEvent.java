701a20929e8071990cc9e8a0bf0c7b43ed495df0
Reece <me@rdil.rocks>
2020-10-28 18:27:24 -0400

    Switch to patching, because its just easier for updating in the future.

--- src/main/java/codes/biscuit/skyblockaddons/events/SkyblockLeftEvent.java
+++
@@ -1,7 +0,0 @@
-package codes.biscuit.skyblockaddons.events;
-
-import cc.hyperium.event.Event;
-
-public class SkyblockLeftEvent extends Event {
-    // This is intentionally empty since there's no useful data we need to include.
-}
